1ebcacfb353a855c2238882d1913244a5fee88ae
Matthew Swartzendruber <matthewswar@gmail.com>
2025-02-27 22:24:37 -0800

    Set default spacing

--- lua/matthewswar/set.lua
+++ lua/matthewswar/set.lua
@@ -7,3 +7,8 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', { fg = '#3b4261' })
+
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
