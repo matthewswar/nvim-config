29c27090fb2cf90f9859f7e74fbfe058b9c841c6
Matthew Swartzendruber <matt.swartzendruber@tanium.com>
2026-03-16 14:58:32 -0700

    Switch from ts_ls to tsgo and add git specific commands

--- init.lua
+++ init.lua
@@ -387,7 +387,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sg', function()
+        builtin.live_grep({ additional_args = { '--hidden' } })
+      end, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -602,7 +604,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        ts_ls = {},
+        -- ts_ls = {},
+        tsgo = {},
         --
 
         lua_ls = {
--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,6 +1,54 @@
+local function run_cmd(command)
+  local handle = io.popen(command)
+  if handle == nil then
+    return nil
+  end
+  local output = handle:read('*a')
+  handle:close()
+  return vim.fn.trim(output)
+end
+
+local function retrieve_blame_hash()
+  local current_line = tostring(vim.fn.getpos('.')[2])
+  local current_file_path_full = vim.fn.expand('%')
+
+  local output = run_cmd('git blame -L ' .. current_line .. ',' .. current_line .. ' "' .. current_file_path_full .. '"')
+  return vim.fn.matchstr(output, '^\\S\\+')
+end
+
 return {
   'tpope/vim-fugitive',
   config = function()
     vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = '[g]it [s]ource' })
+    vim.keymap.set('n', '<leader>gds', '<Cmd>Gvdiffsplit<CR>', { desc = '[g]it [d]iff [s]plit' })
+    vim.keymap.set('n', '<leader>gb', '<Cmd>GBrowse<CR>', { desc = '[g]it [b]rowse' })
+    vim.keymap.set('v', '<leader>gb', function()
+      local start_pos = vim.fn.getpos('v')[2]
+      local end_pos = vim.fn.getpos('.')[2]
+      if start_pos > end_pos then
+        start_pos, end_pos = end_pos, start_pos
+      end
+      vim.cmd(tostring(start_pos) .. ',' .. tostring(end_pos) .. 'GBrowse')
+    end, { desc = '[g]it [b]rowse' })
+    vim.keymap.set('n', '<leader>gc', function()
+      local blame_hash = retrieve_blame_hash()
+      vim.cmd('GBrowse ' .. blame_hash)
+    end, { desc = '[g]it open [c]ommit' })
+    vim.keymap.set('n', '<leader>gp', function()
+      local blame_hash = retrieve_blame_hash()
+      local output = run_cmd('git log -n 1 --no-decorate --oneline ' .. blame_hash)
+
+      local pull_request_number = vim.fn.matchstr(output, [[\v\(#\zs\d+\ze\)$]])
+      if pull_request_number == '' then
+        print('Pull request number not found')
+        return
+      end
+
+      output = run_cmd('git ls-remote --get-url origin')
+      local domain = vim.fn.matchstr(output, [[\vgit\@\zs.*\ze:]])
+      local repo = vim.fn.matchstr(output, [[\vgit\@.*:\zs.*\ze.git]])
+
+      vim.cmd('GBrowse https://' .. domain .. '/' .. repo .. '/pull/' .. pull_request_number)
+    end, { desc = '[g]it open [p]ull request' })
   end,
 }
